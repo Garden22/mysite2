7a7601c4b5c4c106624e486095dbfa44850a69df
Garden22 <dalkomjjang@gmail.com>
2022-06-04 16:48:04 +0900

    220604

--- WebContent/assets/webdb.sql
+++ WebContent/assets/webdb.sql
@@ -22,8 +22,29 @@ UPDATE users
 SET password = 'test'
 WHERE id = '테스트';
 
+DROP TABLE guestbook;
+
+CREATE TABLE guestbook(
+    no NUMBER,
+    name VARCHAR2(80),
+    password VARCHAR2(20),
+    content VARCHAR2(2000),
+    reg_date DATE,
+    PRIMARY KEY (no)
+);
+
+DROP SEQUENCE seq_guestbook_no;
+
+CREATE SEQUENCE seq_guestbook_no
+INCREMENT BY 1
+START WITH 1
+NOCACHE;
+
 COMMIT;
 ROLLBACK;
 
 SELECT * 
 FROM users;
+
+SELECT *
+FROM guestbook;
